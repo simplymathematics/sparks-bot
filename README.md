939f0662da2617bd1aa3bcc9d2d7eebab199ce00
simplymathematics <charliemeyers@gmail.com>
2019-03-07 14:17:49 -0500

    first

---
+++ README.md
@@ -0,0 +1,7 @@
+# esp8266-robot
+
+Build a WiFi Controlled Robot with the ESP8266
+
+# pixels_test
+
+Use an AG833 to view a heat array.
---
+++ esp8266_robot/esp8266_robot.ino
@@ -0,0 +1,176 @@
+// Import required libraries
+#include "ESP8266WiFi.h"
+#include <aREST.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+ 
+// And connect 2 DC motors to port M3 & M4 !
+Adafruit_DCMotor *BL_MOTOR = AFMS.getMotor(3);
+Adafruit_DCMotor *BR_MOTOR = AFMS.getMotor(4);
+Adafruit_DCMotor *FL_MOTOR = AFMS.getMotor(2);
+Adafruit_DCMotor *FR_MOTOR = AFMS.getMotor(1);
+
+
+// Create aREST instance
+aREST rest = aREST();
+
+// WiFi parameters
+const char* ssid = "wiggly electrons";
+const char* password = "fuckthepolice!";
+
+// The port to listen for incoming TCP connections 
+#define LISTEN_PORT           80
+
+// Create an instance of the server
+WiFiServer server(LISTEN_PORT);
+
+// Function
+int stop(String message);
+int forward(String message);
+int right(String message);
+int left(String message);
+int backward(String message);
+
+void setup(void)
+{  
+  // Start Serial
+  Serial.begin(115200);
+
+  // Init motor shield
+  AFMS.begin();  
+
+  // Functions          
+  rest.function("stop", stop);
+  rest.function("forward", forward);
+  rest.function("left", left);
+  rest.function("right", right);
+  rest.function("backward", backward);
+      
+  // Give name and ID to device
+  rest.set_id("1");
+  rest.set_name("robot");
+  
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+  
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+  
+}
+
+void loop() {
+
+  Serial.println("Hello World");
+  
+  // Handle REST calls
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  while(!client.available()){
+    delay(1);
+  }
+  rest.handle(client);
+ 
+}
+
+int stop(String command) {
+  int i = 0;
+  while (i < 50){
+    BL_MOTOR->run( RELEASE );
+    BR_MOTOR->run( RELEASE );
+    FL_MOTOR->run( RELEASE );
+    FR_MOTOR->run( RELEASE );
+    delay(1);
+    i += 1;
+  }
+}
+
+int forward(String command) {
+  BL_MOTOR->setSpeed(200);
+  BR_MOTOR->setSpeed(200);
+  FL_MOTOR->setSpeed(200);
+  FR_MOTOR->setSpeed(200);
+  int i;
+  i = 0 ;
+  while (i < 50){
+    BL_MOTOR->run( FORWARD );
+    BR_MOTOR->run( FORWARD );
+    FL_MOTOR->run( FORWARD );
+    FR_MOTOR->run( FORWARD );
+    delay(1);
+    i = i + 1;
+  
+  }
+}
+
+int left(String command) {
+  
+  BL_MOTOR->setSpeed(100);
+  BR_MOTOR->setSpeed(100);
+  FL_MOTOR->setSpeed(100);
+  FR_MOTOR->setSpeed(100);
+  
+  int i = 0;
+  while (i < 25){
+    BL_MOTOR->run( BACKWARD );
+    BR_MOTOR->run( FORWARD );
+    FL_MOTOR->run( BACKWARD );
+    FR_MOTOR->run( FORWARD );
+    delay(1);
+    i += 1;
+  }
+  
+}
+
+int right(String command) {
+  
+  BL_MOTOR->setSpeed(100);
+  BR_MOTOR->setSpeed(100);
+  FL_MOTOR->setSpeed(100);
+  FR_MOTOR->setSpeed(100);
+
+  int i = 0;
+  while (i < 25){
+    BL_MOTOR->run( FORWARD );
+    BR_MOTOR->run( BACKWARD );
+    FL_MOTOR->run( FORWARD );
+    FR_MOTOR->run( BACKWARD );
+    delay(1);
+    i += 1;
+  }
+  
+}
+int backward(String command) {
+  
+  // Stop
+  BL_MOTOR->setSpeed(150);
+  BR_MOTOR->setSpeed(150);
+  FL_MOTOR->setSpeed(150);
+  FR_MOTOR->setSpeed(150);
+
+  int i = 0;
+  while (i < 50){
+    BL_MOTOR->run( BACKWARD );
+    BR_MOTOR->run( BACKWARD );
+    FL_MOTOR->run( BACKWARD );
+    FR_MOTOR->run( BACKWARD );
+    delay(1);
+    i += 1;
+  }
+  
+}
+  
